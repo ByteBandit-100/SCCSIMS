32c5485f43a62634765e6e87954cdd5ad3b3a29b
ByteBandit-100 <ghsmohit2004@gmail.com>
2026-04-04 12:24:53 +0530

    Apply .gitignore rules

--- server/query.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("sccsims.db")
-cursor = conn.cursor()
-
-cursor.execute("DELETE FROM scan_history WHERE ports IS NULL OR ports=''")
-
-conn.commit()
-conn.close()
